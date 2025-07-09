3efea9098ec47ea1ddccc29c900fb0448cbde49c
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-07-09 15:00:33 +0200

    feat: add class Aquila

---
+++ src/org/lessons/java/animals/Aquila.java
@@ -0,0 +1,48 @@
+package org.lessons.java.animals;
+
+public class Aquila extends Animale {
+    private double flightAltitude; // m
+    private double wingSpan; // cm
+    private double beakLength; // cm
+
+    public Aquila(String species, int age, double flightAltitude, double wingSpan, double beakLength) {
+        super(species, age);
+        this.flightAltitude = flightAltitude;
+        this.wingSpan = wingSpan;
+        this.beakLength = beakLength;
+    }
+
+    public double getFlightAltitude() {
+        return this.flightAltitude;
+    }
+
+    public void setFlightAltitude(double flightAltitude) {
+        this.flightAltitude = flightAltitude;
+    }
+
+    public double getWingSpan() {
+        return this.wingSpan;
+    }
+
+    public void setWingSpan(double wingSpan) {
+        this.wingSpan = wingSpan;
+    }
+
+    public double getBeakLenght() {
+        return this.beakLength;
+    }
+
+    public void setBeakLenght(double beakLength) {
+        this.beakLength = beakLength;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("Kiiiiii-eee");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("The eagle eat meat");
+    }
+}
