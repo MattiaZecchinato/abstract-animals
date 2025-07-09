a9fec94a8e3b400bde17c1bd5d47666cd8f3346d
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-07-09 15:00:55 +0200

    feat: add class Delfino

---
+++ src/org/lessons/java/animals/Delfino.java
@@ -0,0 +1,38 @@
+package org.lessons.java.animals;
+
+public class Delfino extends Animale {
+    private int groupSize;
+    private double swimSpeed; // kph
+
+    public Delfino(String species, int age, int groupSize, double swimSpeed) {
+        super(species, age);
+        this.groupSize = groupSize;
+        this.swimSpeed = swimSpeed;
+    }
+
+    public int getGroupSize() {
+        return this.groupSize;
+    }
+
+    public void setGroupSize(int groupSize) {
+        this.groupSize = groupSize;
+    }
+
+    public double getSwimSpeed() {
+        return this.swimSpeed;
+    }
+
+    public void setSwimSpeed(double swimSpeed) {
+        this.swimSpeed = swimSpeed;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("Whee-whee");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("The dolphine eat fish");
+    }
+}
